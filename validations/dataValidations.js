991a0cc4a2c6304ee322701d6ca5671267cad6e1
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2022-12-08 22:57:44 -0500

    Changed Validation functions and folder structure

--- validations/dataValidations.js
+++ validations/dataValidations.js
@@ -2,6 +2,8 @@
 
 // };
 const { ObjectId } = require("mongodb");
+const emailValidate = require("email-validator");
+
 const validateID = (id) => {
   if (!id) {
     throw "All fields need to have valid values";
@@ -18,361 +20,99 @@ const validateID = (id) => {
   }
 };
 
-function userNameValidation(userName) {
-    if (userName == undefined|| userName == null) {
-        throw 'userName cannot be undefined'
-    }
-    if (typeof userName !== "string") {
-        throw `Error:userName must be a string`;
-      }
-  userName = userName.trim();
-  if (userName.length === 0) {
-    throw `Error: userName cannot be an empty string or just spaces`;
+const validateUsername = (inputUsername) => {
+  if (!inputUsername) throw "Username not provided.";
+  if (typeof inputUsername !== "string") throw "Username is not of valid input type.";
+  if (inputUsername.trim().length === 0) throw "Username is empty.";
+  if (inputUsername.includes(" ")) throw "Username should not contain spaces.";
+  if (inputUsername.length < 4) throw "Username must contain at least 4 characters.";
+  const regex = new RegExp("^[a-zA-Z0-9]*$");
+  if (!regex.test(inputUsername)) throw "Username should contain only alphanumeric characters.";
+};
+
+const validatePassword = (inputPassword) => {
+  if (!inputPassword) throw "Password not provided.";
+  if (typeof inputPassword !== "string") throw "Password is not of valid input type.";
+  if (inputPassword.trim().length === 0) throw "Password contains only whitespaces.";
+  if (inputPassword.includes(" ")) throw "Password should not contain spaces.";
+  if (inputPassword.length < 6) throw "Password should contain at least 6 characters.";
+  const regexDigit = /[0-9]/;
+  if (inputPassword.search(regexDigit) < 0) {
+    throw "Password should contain at least one number";
   }
-  if (userName === undefined) {
-    throw `Error: You must provide a userName`;
+  const regexUppercase = /[A-Z]/;
+  if (inputPassword.search(regexUppercase) < 0) {
+    throw "Password should contain at least one uppercase character";
   }
-}
-function passwordValidation(password) {
-    if (password === undefined || password === null) {
-        throw `Error: password cannot be null and undefined`;
-      }
-      if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,}$/)) {
-       throw `Error: Password must contain atleast one upper case,one lower case,one digit, one special character and should be  atleast eight characters long`
-    }
-}
-function UserValidation(
-  userName,
-  firstName,
-  lastName,
-  email,
-  password,
-  contactNumber,
-  gender,
-  dob,
-  preferences
-) {
-if (userName == undefined|| userName == null) {
-        throw 'userName cannot be undefined'
-}
-if (typeof userName !== "string") {
-        throw `Error:userName must be a string`;
-}
-  userName = userName.trim();
-  if (userName.length === 0) {
-    throw `Error: userName cannot be an empty string or just spaces`;
+  const regexSpecialCharacter = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (inputPassword.search(regexSpecialCharacter) < 0) {
+    throw "Password should contain at least one special character";
   }
-  if (firstName === undefined) {
-    throw `Error: You must provide a firstName`;
+};
+
+const validateName = (inputName) => {
+  if (!inputName) throw "Name not provided.";
+  if (typeof inputName !== "string") throw "Name is not of valid input type.";
+  if (inputName.trim().length === 0) throw "Name is empty.";
+  if (inputName.includes(" ")) throw "Name should not contain spaces.";
+  if (inputName.length < 2) throw "Name must contain at least 4 characters.";
+  const regex = new RegExp("^[a-zA-Z0-9]*$");
+  if (!regex.test(inputName)) throw "Name should contain only alphanumeric characters.";
+};
+
+const validateEmail = (inputEmail) => {
+  email = inputEmail.trim();
+  let checkEmail = emailValidate.validate(email);
+  if (checkEmail === false) throw "Invalid email format!";
+};
+
+const validateNumber = (inputNum) => {
+  if (isNaN(inputNum)) throw "Date is not a valid number!";
+  if (inputNum % 1 !== 0) throw "Date cannot be a decimal ID!";
+  return inputNum;
+};
+const validateDOB = (inputdate) => {
+  if (!inputdate) {
+    throw "Input date of birth is empty";
   }
-  if (typeof firstName !== "string") {
-    throw `Error:firstName must be a string`;
-  }
-  firstName = firstName.trim();
-  if (firstName.length === 0) {
-    throw `Error:firstName cannot be an empty string or just spaces`;
-  }
-  if (!firstName.match(/^[a-zA-Z\s]+$/)) {
-    throw "FirstName must be alphabets";
-  }
-  if (lastName === undefined) {
-    throw `Error: You must provide a lastName`;
-  }
-  if (typeof lastName !== "string") {
-    throw `Error:lastName must be a string`;
-  }
-  lastName = lastName.trim();
-  if (lastName.length === 0) {
-    throw `Error: lastName cannot be an empty string or just spaces`;
-  }
-  if (!lastName.match(/^[a-zA-Z\s]+$/)) {
-    throw "lastName must be alphabets";
-  }
-  if (userName === undefined) {
-    throw `Error: You must provide a userName`;
-  }
-  if (typeof userName !== "string") {
-    throw `Error: userName must be a string`;
-  }
-  if (email === undefined || email === null) {
-    throw `Error: emailAddress cannot be null and undefined`;
-  }
-  if (typeof email !== "string") {
-    throw `Error:emailAddress must be a string`;
+  date = inputdate.trim();
+  if (date.length !== 10) throw "Incorrect date format";
+  if (date.charAt(4) !== "-" || date.charAt(7) !== "-") throw "Incorrect date format";
+  const currentYear = new Date().getFullYear();
+  const monthKey = { 1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31 };
+  let month = parseInt(validateNumber(date.slice(5, 7)));
+  let day = parseInt(validateNumber(date.slice(8)));
+  let year = parseInt(validateNumber(date.slice(0, 4)));
+  if (month < 1 || month > 12) throw "Invalid month";
+  if (year < 1900 || year > currentYear) throw "Invalid year";
+  if (day > monthKey[month] || day < 1) {
+    throw "Invalid day";
   }
 
-  email = email.trim();
-  if (!email.match(/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/)) {
-    throw `Error: Please enter a valid emailAddress`;
-  }
-  if (email.trim().length == 0) {
-    throw "Email address cannot be empty";
-  }
-  if (password === undefined || password === null) {
-    throw `Error: password cannot be null and undefined`;
-  }
-  if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,}$/)) {
-   throw `Error: Password must contain atleast one upper case,one lower case,one digit, one special character and should be  atleast eight characters long`
-}
-  contactNumber = contactNumber.trim();
-  if (!contactNumber.match(/^\d{10}$/)) {
-    throw `Error:Please enter a valid contact number`;
-  }
-  if (gender == undefined) {
-    throw `Error: You must provide your gender`;
-  }
-  if (typeof gender !== "string" || gender.trim().length === 0) {
-    throw `gender is not a string or is an empty string`;
-  }
-  gender = gender.trim();
+  let currentDay = new Date();
+  let birth = new Date(date);
+  let years = Math.abs(currentDay.getTime() - birth.getTime()) / 1000 / 60 / 60 / 24 / 365;
+  if (years < 16) throw "Must be at least 16 years old to signup!";
+};
 
-  if (gender != "male" && gender != "female" && gender != "other") {
-    throw "gender values must be 'male','female','other'";
+const validatePhoneNumber = (inputPhoneNumber) => {
+  if (!inputPhoneNumber) {
+    throw "Input Phone number is empty";
   }
-  let year = "";
-  let arr = [];
+  inputPhoneNumber = inputPhoneNumber.trim();
+  if (inputPhoneNumber.length !== 10) {
+    throw "Phone number must contain 10 digits";
+  }
+  const regex = new RegExp("^[0-9]*$");
+  if (!regex.test(inputPhoneNumber)) throw "Phone number should contain only digits.";
+};
 
-  for (let j = dob.length; j > 0; j--) {
-    if (dob[j] == undefined) {
-      continue;
-    }
-    if (dob[j] == "/") {
-      break;
-    } else {
-      arr.push(dob[j]);
-    }
-  }
-  for (let k = arr.length; k >= 0; k--) {
-    if (arr[k] == undefined) {
-      continue;
-    } else {
-      year += arr[k];
-    }
-  }
-  var currentdate = new Date();
-  var currentDate =
-    currentdate.getMonth() +
-    1 +
-    "/" +
-    currentdate.getDate() +
-    "/" +
-    currentdate.getFullYear();
-  currentDate.split("/");
-  let age;
-  age = currentdate.getFullYear() - Number(year);
-  if (age < 16) {
-    throw "You must be atleast 16 to register";
-  }
-  let isValidDate = Date.parse(dob);
-  if (isNaN(isValidDate)) {
-    throw "Not a valid date format";
-  }
-  preferences = preferences.trim();
-  if (preferences == undefined) throw `Error: You must provide  preferences`;
-  // Need to check this
-  // if (!(Array.isArray(preferences))) {
-  //     throw `Error:${preferences} must be an array`;
-  // }
-  let invalidarrayflag = false;
-  if (typeof preferences !== "string" || preferences.trim().length === 0) {
-    throw `One or more elements in preferences array is not a string or is an empty string`;
-  }
-}
-function EmployeeValidation(
-  userName,
-  preferences,
-  resume,
-  historyofJobs,
-  overallRating,
-  reported,
-  currentJobsTaken,
-  invites,
-  wishlist
-) {
-  if (userName === undefined) {
-    throw `Error: You must provide a userName`;
-  }
-  if (typeof userName !== "string") {
-    throw `Error:userName must be a string`;
-  }
-  userName = userName.trim();
-  if (userName.length === 0) {
-    throw `Error: userName cannot be an empty string or just spaces`;
-  }
-  if (preferences == undefined) throw `Error: You must provide  preferences`;
-  if (!Array.isArray(preferences)) {
-    throw `Error:preferences must be an array`;
-  }
-  let arrayInvalidFlag = false;
-  for (i in preferences) {
-    if (
-      typeof preferences[i] !== "string" ||
-      preferences[i].trim().length === 0
-    ) {
-      arrayInvalidFlag = true;
-      break;
-    }
-    preferences[i] = preferences[i].trim();
-  }
-  if (arrayInvalidFlag) {
-    throw `One or more elements in preferences array is not a string or is an empty string`;
-  }
-  if (resume === undefined) throw `Error: You must provide a resume`;
-  if (typeof resume !== "string" && !(resume === null)) {
-    throw `Error:resume must be a string`;
-  }
-  resume = resume.trim();
-  if (resume.length === 0) {
-    throw `Error: resume cannot be an empty id or just spaces`;
-  }
-  if (!ObjectId.isValid(resume)) {
-    throw `Error: resume is an invalid object ID`;
-  }
-  if (wishlist == undefined) throw `Error: You must provide  wishlist`;
-  if (!Array.isArray(wishlist)) {
-    throw `Error:wishlist must be an array`;
-  }
-  for (i in wishlist) {
-    if (typeof wishlist[i] !== "string" || wishlist[i].trim().length === 0) {
-      arrayInvalidFlag = true;
-      break;
-    }
-    wishlist[i] = wishlist[i].trim();
-  }
-  if (arrayInvalidFlag) {
-    throw `One or more elements in wishlist array is not a string or is an empty string`;
-  }
-
-  if (!Array.isArray(historyofJobs)) {
-    throw `Error:${historyofJobs} must be an array`;
-  }
-  if (historyofJobs.length > 0) {
-    for (i in historyofJobs) {
-      if (
-        typeof historyofJobs[i] !== "string" ||
-        historyofJobs[i].trim().length === 0
-      ) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      historyofJobs[i] = historyofJobs[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in historyofJobs array is not a string or is an empty string`;
-    }
-  }
-  overallRating = Number(overallRating);
-
-  if (
-    overallRating % 1 != 0 &&
-    (overallRating < 0 || overallRating > 5) &&
-    (overallRating < 1.5 || overallRating > 4.8)
-  ) {
-    throw "rating should be an integer in the range 1-5 or float values between 1.5 and 4.8";
-  }
-  if (!Array.isArray(currentJobsTaken)) {
-    throw `Error:currentJobsTaken must be an array`;
-  }
-  if (currentJobsTaken.length > 0) {
-    for (i in currentJobsTaken) {
-      if (typeof reported[i] !== "string" || reported[i].trim().length === 0) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      reported[i] = reported[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in reported array is not a string or is an empty string`;
-    }
-  }
-
-  if (!Array.isArray(reported)) {
-    throw `Error:reported must be an array`;
-  }
-  if (reported.length > 0) {
-    for (i in reported) {
-      if (typeof reported[i] !== "string" || reported[i].trim().length === 0) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      reported[i] = reported[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in reported array is not a string or is an empty string`;
-    }
-  }
-
-  if (!Array.isArray(reported)) {
-    throw `Error:${reported} must be an array`;
-  }
-  if (invites.length > 0) {
-    for (i in invites) {
-      if (typeof invites[i] !== "string" || invites[i].trim().length === 0) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      invites[i] = invites[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in invites array is not a string or is an empty string`;
-    }
-  }
-}
-function EmployerValidation(historyOfJobs, overallRating, reported) {
-  if (historyOfJobs == undefined) throw `Error: You must provide  preferences`;
-  if (!Array.isArray(preferences)) {
-    throw `Error:preferences must be an array`;
-  }
-  let arrayInvalidFlag = false;
-  if (historyOfJobs.length > 0) {
-    for (i in historyOfJobs) {
-      if (
-        typeof historyOfJobs[i] !== "string" ||
-        historyOfJobs[i].trim().length === 0
-      ) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      historyOfJobs[i] = historyOfJobs[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in historyOfJobs array is not a string or is an empty string`;
-    }
-  }
-  overallRating = Number(overallRating);
-
-  if (
-    (overallRating % 1 != 0 && (overallRating < 1 || overallRating > 5)) ||
-    overallRating < 1.5 ||
-    overallRating > 4.8
-  ) {
-    throw "rating should be an integer in the range 1-5 or float values between 1.5 and 4.8";
-  }
-  if (!Array.isArray(reported)) {
-    throw `Error:${reported} must be an array`;
-  }
-  if (reported.length > 0) {
-    for (i in reported) {
-      if (typeof reported[i] !== "string" || reported[i].trim().length === 0) {
-        arrayInvalidFlag = true;
-        break;
-      }
-      reported[i] = reported[i].trim();
-    }
-    if (arrayInvalidFlag) {
-      throw `One or more elements in reported array is not a string or is an empty string`;
-    }
-  }
-
-  if (!Array.isArray(reported)) {
-    throw `Error:${reported} must be an array`;
-  }
-}
 module.exports = {
   validateID,
-  UserValidation,
-  EmployeeValidation,
-  EmployerValidation,
-  userNameValidation,
-  passwordValidation,
+  validateUsername,
+  validatePassword,
+  validateName,
+  validateEmail,
+  validateDOB,
+  validatePhoneNumber,
 };
